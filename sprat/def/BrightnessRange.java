d5f4b1ad7a3a543a00a61bea89a7477501ac03e9
stollf06 <stollf06>
2009-04-23 13:33:27 +0000

    no message

---
+++ sprat/def/BrightnessRange.java
@@ -0,0 +1,21 @@
+//$Header: /home/xubuntu/berlios_backup/github/tmp-cvs/sprat/Repository/sprat/def/BrightnessRange.java,v 1.1 2009/04/23 13:33:27 stollf06 Exp $
+package def;
+
+public class BrightnessRange {
+
+	public int min;
+	public int max;
+	
+	public BrightnessRange(){
+		
+	}
+	public BrightnessRange(int min, int max){
+		this.min = min;
+		this.max = max;
+	}
+}
+
+//$Log: BrightnessRange.java,v $
+//Revision 1.1  2009/04/23 13:33:27  stollf06
+//no message
+//
