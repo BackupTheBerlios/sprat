3a0551ec20b600a1ad4b9183aef672cc42b29a7e
mahanja <mahanja>
2009-04-23 19:02:10 +0000

    The format had changed, this is the update for the commentary

--- sprat/com/msg/Message.java
+++ sprat/com/msg/Message.java
@@ -1,4 +1,4 @@
-// $Header: /home/xubuntu/berlios_backup/github/tmp-cvs/sprat/Repository/sprat/com/msg/Message.java,v 1.1 2009/04/23 12:08:34 mahanja Exp $
+// $Header: /home/xubuntu/berlios_backup/github/tmp-cvs/sprat/Repository/sprat/com/msg/Message.java,v 1.2 2009/04/23 19:02:10 mahanja Exp $
 
 package com.msg;
 
@@ -6,8 +6,7 @@ public interface Message {
 	
 	/**
 	 * The format is "type:value[;value]", where the values will be replaced by 
-	 * their appropriate values. Values with subvalues are enclosed by brackets 
-	 * ( "(" and ")" ).
+	 * their appropriate values.
 	 * @return the String to be send over bluetooth
 	 */
 	public String getMessageString();
